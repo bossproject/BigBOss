4fd7e8d4a1e97e4a2ed5d00189724234db705dae
Milos Stankovic <milos.stankovic.89@outlook.com>
2015-11-05 23:10:56 +0100

    Dodat AdminPanel kontroler i Project model

---
+++ BigBoss/BigBoss/Controllers/AdminPanelController.cs
@@ -0,0 +1,48 @@
+﻿using BigBoss.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigBoss.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: AdminPanel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //----------- Category controll -----------
+
+        public ActionResult CategoryIndex()
+        {
+            var lista = db.Category.ToArray();
+            return View(lista);
+        }
+
+        public ActionResult CategoryCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CategoryCreate(CategoryModel catModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Category.Add(catModel);
+                await db.SaveChangesAsync();
+                TempData["poruka"] = "Category saved!";
+                return RedirectToAction("CategoryIndex");
+            }
+            return View();
+        }
+
+    }
+}
--- BigBoss/BigBoss/Models/IdentityModels.cs
+++ BigBoss/BigBoss/Models/IdentityModels.cs
@@ -31,5 +31,6 @@ namespace BigBoss.Models
         }
 
         public DbSet<CategoryModel> Category { get; set; }
+        public DbSet<ProjectModel> Project { get; set; }
     }
 }
