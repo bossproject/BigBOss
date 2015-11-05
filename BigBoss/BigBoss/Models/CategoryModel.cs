b96214e75cc53159c8ce0a1608573ee37d0446d9
Milos Stankovic <milos.stankovic.89@outlook.com>
2015-11-05 22:48:08 +0100

    Dodan CategoryModel

---
+++ BigBoss/BigBoss/Models/CategoryModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace BigBoss.Models
+{
+    public class CategoryModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Category name")]
+        public string nameCategory { get; set; }
+
+        [Display(Name = "Category description")]
+        [Required]
+        public string descriptionCategory { get; set; }
+    }
+}
