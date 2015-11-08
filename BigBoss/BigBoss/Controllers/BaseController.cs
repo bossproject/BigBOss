330f11c7078a35737c2cdec663a3fc9ec86c86a4
Filip Jovanovic <fil.93@hotmail.com>
2015-11-08 10:52:49 +0100

    Added BaseController class

---
+++ BigBoss/BigBoss/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigBoss.Controllers
+{
+
+    /*
+    Sadrži sve metode i atribute koji su zajednički za kontrolere. Ali trenutno ne sadrži ništa.
+    #DEAL WITH IT 
+    #LAZYDEVELOPER
+    */
+
+    public class BaseController : Controller
+    {
+        // GET: Base
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
