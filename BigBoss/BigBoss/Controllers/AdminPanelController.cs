c1297d8c7ef932a1b9ae8c18f972f04e3324396b
Milos Stankovic <milos.stankovic.89@outlook.com>
2015-11-11 15:12:07 +0100

    Edit Project

--- BigBoss/BigBoss/Controllers/AdminPanelController.cs
+++ BigBoss/BigBoss/Controllers/AdminPanelController.cs
@@ -8,29 +8,36 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
-namespace BigBoss.Controllers {
-    public class AdminPanelController : Controller {
+namespace BigBoss.Controllers
+{
+    public class AdminPanelController : Controller
+    {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: AdminPanel
-        public ActionResult Index() {
+        public ActionResult Index()
+        {
             return View();
         }
 
         //----------- Category controll -----------
 
-        public ActionResult CategoryIndex() {
+        public ActionResult CategoryIndex()
+        {
             var lista = db.Category.ToArray();
             return View(lista);
         }
 
-        public ActionResult CategoryCreate() {
+        public ActionResult CategoryCreate()
+        {
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> CategoryCreate(CategoryModel catModel) {
-            if(ModelState.IsValid) {
+        public async Task<ActionResult> CategoryCreate(CategoryModel catModel)
+        {
+            if (ModelState.IsValid)
+            {
                 db.Category.Add(catModel);
                 await db.SaveChangesAsync();
                 TempData["success_msg"] = "Category saved!";
@@ -39,8 +46,10 @@ namespace BigBoss.Controllers {
             return View();
         }
 
-        public ActionResult CategoryEdit(int? id) {
-            if(id == null) {
+        public ActionResult CategoryEdit(int? id)
+        {
+            if (id == null)
+            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             var cat = db.Category.Where(c => c.Id == id).FirstOrDefault();
@@ -49,8 +58,10 @@ namespace BigBoss.Controllers {
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> CategoryEdit([Bind(Include = "Id, nameCategory, descriptionCategory")] CategoryModel cat) {
-            if(ModelState.IsValid) {
+        public async Task<ActionResult> CategoryEdit([Bind(Include = "Id, nameCategory, descriptionCategory")] CategoryModel cat)
+        {
+            if (ModelState.IsValid)
+            {
                 db.Entry(cat).State = EntityState.Modified;
                 await db.SaveChangesAsync();
                 TempData["success_msg"] = "Category saved!";
@@ -60,8 +71,9 @@ namespace BigBoss.Controllers {
             return View();
         }
 
-        public ActionResult CategoryDelete(int? id) {
-            if(id == null)
+        public ActionResult CategoryDelete(int? id)
+        {
+            if (id == null)
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             var cat = db.Category.Where(c => c.Id == id).FirstOrDefault();
 
@@ -70,7 +82,8 @@ namespace BigBoss.Controllers {
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> CategoryDelete(int id) {
+        public async Task<ActionResult> CategoryDelete(int id)
+        {
             var cat = db.Category.Where(c => c.Id == id).FirstOrDefault();
             db.Category.Remove(cat);
             await db.SaveChangesAsync();
@@ -80,20 +93,49 @@ namespace BigBoss.Controllers {
 
         //----------- Project controll ------------
 
-        public ActionResult ProjectIndex() {
-            var projectList = db.Project.ToList();
+        public ActionResult ProjectIndex()
+        {
+            var projectList = db.Project.Include(c => c.categoryMod).ToList();
             return View(projectList);
         }
 
-        public ActionResult ProjectCreate() {
+        public ActionResult ProjectCreate()
+        {
             ViewBag.CatModel = new SelectList(db.Category.ToList(), "Id", "nameCategory");
             return View();
         }
 
-        //    [HttpPost]
-        //    [ValidateAntiForgeryToken]
-        //    public async Task<ActionResult> ProjectCreate(ProjectModel modelProj, int? idCat) {
-        //        modelProj.Id = Guid.NewGuid().ToString();
-        //}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ProjectCreate(ProjectModel modelProj, int? categoryMod)
+        {
+            modelProj.Id = Guid.NewGuid().ToString();
+            modelProj.categoryMod = db.Category.Where(c => c.Id == categoryMod).FirstOrDefault();
+            modelProj.moneyWithCommission = (modelProj.money/100)*104;
+            modelProj.moneyRaised = 0;
+            modelProj.numberOfDonations = 0;
+     //       if (ModelState.IsValid)
+     //       {
+                db.Project.Add(modelProj);
+                await db.SaveChangesAsync();
+                return RedirectToAction("ProjectIndex");
+     //       }
+     //       return View();
+        }
+
+        public async Task<ActionResult> ProjectEdit(string id)
+        {
+            ProjectModel mod = await db.Project.FindAsync(id);
+            //    var lista = new SelectList(db.Category.ToArray(), "Id", "nameCategory");
+            //    ViewBag.CatModel = lista;
+            ProjectEditViewModel noviMod = new ProjectEditViewModel();
+            noviMod.listaKaregorija = db.Category.ToList();
+            noviMod.categoryMod = mod.categoryMod;
+            noviMod.nameProject = mod.nameProject;
+           // model.OrderTemplates = new SelectList(db.OrderTemplates, "OrderTemplateId", "OrderTemplateName", 1);
+            return View(noviMod);
+        }
+
+
     }
 }
---
+++ BigBoss/BigBoss/Models/AdminPanelViewModels.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigBoss.Models
+{
+    public class ProjectEditViewModel
+    {
+        [Required]
+        [Display(Name = "Name of project")]
+        public string nameProject { get; set; }
+
+        [Required]
+        [Display(Name = "Description of project")]
+        public string descProject { get; set; }
+
+        [Display(Name = "Additionl info")]
+        public string additionalInfo { get; set; }
+
+        [Display(Name = "Tags")]
+        [Required]
+        public string tagsProject { get; set; }
+
+        [Display(Name = "Money needed for donation")]
+        [Required]
+        public decimal money { get; set; }
+
+        [Display(Name = "Money with commision")]
+        public decimal moneyWithCommission { get; set; }
+
+        [Display(Name = "Money raised")]
+        public decimal moneyRaised { get; set; }
+
+        [Display(Name = "Number of donations")]
+        public int numberOfDonations { get; set; }
+
+        [Display(Name = "Category")]
+        public CategoryModel categoryMod { get; set; }
+
+        public IEnumerable<CategoryModel> listaKaregorija { get; set; }
+    }
+}
