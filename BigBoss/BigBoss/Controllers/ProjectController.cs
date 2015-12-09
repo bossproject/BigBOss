a55d8d7591a2b0d1a6994c202cd3bf35004e798f
Filip Jovanovic <fil.93@hotmail.com>
2015-12-09 22:32:41 +0100

    Search

---
+++ BigBoss/BigBoss/Controllers/ProjectController.cs
@@ -0,0 +1,24 @@
+﻿using BigBoss.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace BigBoss.Controllers {
+    public class ProjectController : Controller {
+        public ApplicationDbContext db { get { return HttpContext.GetOwinContext().Get<ApplicationDbContext>(); } }
+        // GET: Project
+        public async Task<ActionResult> Show(string id) {
+            return View(await db.Project.FindAsync(id));
+        }
+
+        public ActionResult Search(string q) {
+            var list = db.Project.Where(p => p.nameProject.Contains(q) || p.tagsProject.Contains(q) || p.categoryMod.nameCategory.Contains(q)).ToList();
+            return View(list);
+        }
+    }
+}
